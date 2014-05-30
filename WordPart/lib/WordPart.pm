6f1969e187584f89e8d3af6d5c1e168ad20a92c9
Gene Boggs <gene.boggs@gmail.com>
2014-05-29 21:06:01 -0400

    WIP: parsing...

--- WordPart/lib/WordPart.pm
+++ WordPart/lib/WordPart.pm
@@ -10,9 +10,14 @@ use WordPart::DataObjects::Fragment::Manager;
 use WordPart::Forms::Fragment;
 use WordPart::Tables::Fragment;
 
+use Data::Dumper::Concise;
+
+use lib '/Users/gene/sandbox/github/ology/Lingua-Word-Parser/lib';
+use Lingua::Word::Parser;
+
 =head1 NAME
 
-WordPart - Partitions of words
+WordPart - Partition a word into scored, known fragments
 
 =head1 DEBUGGING
 
@@ -66,6 +71,30 @@ sub edit_part
             %args
         )->handle_request;
     }
+
+    return;
+}
+
+sub parse_word {
+    my ( $self, %args ) = @_;
+
+    my $p = Lingua::Word::Parser->new(
+        word   => $args{word},
+        dbname => 'word_part',
+        dbuser => 'root',
+        dbpass => 'abc123',
+    );
+    #warn Dumper $p->{lex};
+
+    # Find the known word-part positions.
+    my ($known) = $p->knowns;
+    my $combos = $p->power;
+    my $score = $p->score;
+    #warn Dumper $score;
+#    warn Dumper $score->{ [ sort keys $score ]->[-1] };
+
+#    $self->tree_append( tag => 'score', data => $score );
+    $self->fast_append( tag => 'score', data => { dump => Dumper($score) } );
 
     return;
 }
