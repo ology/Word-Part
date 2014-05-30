f8c6b7f8a50260b9eb2e5b7c7f0d22645162b740
Gene Boggs <gene.boggs@gmail.com>
2014-05-30 07:47:34 -0400

    Removed pronunciation column

--- WordPart/lib/WordPart/DataObjects/Fragment.pm
+++ WordPart/lib/WordPart/DataObjects/Fragment.pm
@@ -9,11 +9,10 @@ __PACKAGE__->meta->setup(
     table  => 'fragment',
 
     columns => [
-        id            => { type => 'serial', not_null => 1 },
-        affix         => { type => 'varchar', default => '', length => 50, not_null => 1 },
-        definition    => { type => 'varchar', default => '', length => 255, not_null => 1 },
-        etymology     => { type => 'varchar', length => 50 },
-        pronunciation => { type => 'varchar', length => 50 },
+        id         => { type => 'serial', not_null => 1 },
+        affix      => { type => 'varchar', default => '', length => 50, not_null => 1 },
+        definition => { type => 'varchar', default => '', length => 255, not_null => 1 },
+        etymology  => { type => 'varchar', length => 50 },
     ],
 
     primary_key_columns => [ 'id' ],
